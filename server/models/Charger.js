7e295cf4908b7e45f3c36a0584171b2e974eeecb
yxx-dev <hermitlun@gmail.com>
2022-12-08 19:55:52 -0800

    models

---
+++ server/models/Charger.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const chargerSchema = new Schema({
+  chargerId: {
+    type: String,
+    required,
+    trim: true,
+  },
+  portId: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  // comments: [
+  //   {
+  //     commentText: {
+  //       type: String,
+  //       required: true,
+  //       minlength: 1,
+  //       maxlength: 280,
+  //     },
+  //     commentAuthor: {
+  //       type: String,
+  //       required: true,
+  //     },
+  //     createdAt: {
+  //       type: Date,
+  //       default: Date.now,
+  //       get: (timestamp) => dateFormat(timestamp),
+  //     },
+  //   },
+  // ],
+});
+
+const Charger = model('Charger', chargerSchema);
+
+module.exports = Charger;
