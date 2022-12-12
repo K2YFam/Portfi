4d565963129fcb7cc961eadca45e27c4b49a34e3
yxx-dev <hermitlun@gmail.com>
2022-12-11 20:07:43 -0800

    login ok

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,18 +25,32 @@ export const ADD_USER = gql`
 `;
 
 export const ADD_THOUGHT = gql`
-mutation Mutation($chargerId: String!, $portId: String!) {
-  addCharger(chargerId: $chargerId, portId: $portId) {
-    _id
-    chargerOwner
-    chargerId
-    portId
-    createdAt
+  mutation addThought($thoughtText: String!) {
+    addThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      thoughtAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+      }
+    }
   }
-}
-
 `;
 
 export const ADD_COMMENT = gql`
- 
+  mutation addComment($thoughtId: ID!, $commentText: String!) {
+    addComment(thoughtId: $thoughtId, commentText: $commentText) {
+      _id
+      thoughtText
+      thoughtAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        createdAt
+      }
+    }
+  }
 `;
