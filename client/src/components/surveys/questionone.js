d06076eb81fc95b945f55a8153a20f7b3024f5cc
Phanimal <jameslphan@hotmail.com>
2022-12-12 19:36:34 -0800

    survey upgrade

--- client/src/components/surveys/questionone.js
+++ client/src/components/surveys/questionone.js
@@ -18,11 +18,50 @@ const json = {
        },
        {
         "value": "Item 2",
-        "text": "Vacation Charge Mode"
+        "text": "Roadtrip Charge Mode"
        },
        {
         "value": "Item 3",
         "text": "Pika Thunder Mode"
+       }
+      ]
+     },
+     {
+      "type": "checkbox",
+      "name": "question6",
+      "title": "Favorite Bri-ish saying: (Select all that apply)",
+      "choices": [
+       {
+        "value": "Item 1",
+        "text": "Aloe Love"
+       },
+       {
+        "value": "Item 2",
+        "text": "Oit there mate, bit rude to put that knoife in me chest innit?"
+       },
+       {
+        "value": "Item 3",
+        "text": "Roight wots oll dis den?"
+       },
+       {
+        "value": "Item 5",
+        "text": "The Bee-ools"
+       },
+       {
+        "value": "Item 6",
+        "text": "Fank yew"
+       },
+       {
+        "value": "Item 7",
+        "text": "I'm not bovered"
+       },
+       {
+        "value": "Item 8",
+        "text": "Arry pah uh"
+       },
+       {
+        "value": "Item 9",
+        "text": "Peta Paka"
        }
       ]
      }
@@ -34,284 +73,53 @@ const json = {
     "name": "page2",
     "elements": [
      {
-      "type": "dropdown",
+      "type": "radiogroup",
       "name": "question2",
-      "title": "What time would like to start your charge?",
+      "title": "Pikachu use:",
       "choices": [
        {
         "value": "Item 1",
-        "text": "1:00 am"
+        "text": "Thunder Shock"
        },
        {
         "value": "Item 2",
-        "text": "2:00 am"
+        "text": "Spark"
        },
        {
         "value": "Item 3",
-        "text": "3:00 am"
-       },
-       {
-        "value": "Item 4",
-        "text": "4:00 am"
-       },
-       {
-        "value": "Item 5",
-        "text": "5:00 am"
-       },
-       {
-        "value": "Item 6",
-        "text": "6:00 am"
-       },
-       {
-        "value": "Item 7",
-        "text": "7:00 am"
-       },
-       {
-        "value": "Item 8",
-        "text": "8:00 am"
-       },
-       {
-        "value": "Item 9",
-        "text": "9:00 am"
-       },
-       {
-        "value": "Item 10",
-        "text": "10:00 am"
-       },
-       {
-        "value": "Item 11",
-        "text": "11:00 am"
-       },
-       {
-        "value": "Item 12",
-        "text": "12:00 am"
-       },
-       {
-        "value": "Item 13",
-        "text": "1:00 pm"
-       },
-       {
-        "value": "Item 14",
-        "text": "2:00 pm"
-       },
-       {
-        "value": "Item 15",
-        "text": "3:00 pm"
-       },
-       {
-        "value": "Item 16",
-        "text": "4:00 pm"
-       },
-       {
-        "value": "Item 17",
-        "text": "5:00 pm"
-       },
-       {
-        "value": "Item 18",
-        "text": "6:00 pm"
-       },
-       {
-        "value": "Item 19",
-        "text": "7:00 pm"
-       },
-       {
-        "value": "Item 20",
-        "text": "8:00 pm"
-       },
-       {
-        "value": "Item 21",
-        "text": "9:00 pm"
-       },
-       {
-        "value": "Item 22",
-        "text": "10:00 pm"
-       },
-       {
-        "value": "Item 23",
-        "text": "11:00 pm"
-       },
-       {
-        "value": "Item 24",
-        "text": "12:00 pm"
-       }
-      ],
-      "noneText": "80% Charged",
-      "otherText": "100% Charged"
-     },
-     {
-      "type": "dropdown",
-      "name": "question4",
-      "title": "What time would like to stop your charge?",
-      "choices": [
-       {
-        "value": "Item 1",
-        "text": "1:00 am"
-       },
-       {
-        "value": "Item 2",
-        "text": "2:00 am"
-       },
-       {
-        "value": "Item 3",
-        "text": "3:00 am"
-       },
-       {
-        "value": "Item 4",
-        "text": "4:00 am"
-       },
-       {
-        "value": "Item 5",
-        "text": "5:00 am"
-       },
-       {
-        "value": "Item 6",
-        "text": "6:00 am"
-       },
-       {
-        "value": "Item 7",
-        "text": "7:00 am"
-       },
-       {
-        "value": "Item 8",
-        "text": "8:00 am"
-       },
-       {
-        "value": "Item 9",
-        "text": "9:00 am"
-       },
-       {
-        "value": "Item 10",
-        "text": "10:00 am"
-       },
-       {
-        "value": "Item 11",
-        "text": "11:00 am"
-       },
-       {
-        "value": "Item 12",
-        "text": "12:00 am"
-       },
-       {
-        "value": "Item 13",
-        "text": "1:00 pm"
-       },
-       {
-        "value": "Item 14",
-        "text": "2:00 pm"
-       },
-       {
-        "value": "Item 15",
-        "text": "3:00 pm"
-       },
-       {
-        "value": "Item 16",
-        "text": "4:00 pm"
-       },
-       {
-        "value": "Item 17",
-        "text": "5:00 pm"
-       },
-       {
-        "value": "Item 18",
-        "text": "6:00 pm"
-       },
-       {
-        "value": "Item 19",
-        "text": "7:00 pm"
-       },
-       {
-        "value": "Item 20",
-        "text": "8:00 pm"
-       },
-       {
-        "value": "Item 21",
-        "text": "9:00 pm"
-       },
-       {
-        "value": "Item 22",
-        "text": "10:00 pm"
-       },
-       {
-        "value": "Item 23",
-        "text": "11:00 pm"
-       },
-       {
-        "value": "Item 24",
-        "text": "12:00 pm"
-       },
-       {
-        "value": "Item 25",
-        "text": "80% Charged"
-       },
-       {
-        "value": "Item 26",
-        "text": "100% Charged"
-       },
-       {
-        "value": "Item 27",
-        "text": "Only when the insatiable Thor's Mjolnir can be ,... satiated!"
+        "text": "Quick Attack"
        }
       ]
      }
-    ],
-    "title": "Portfi Charge Start/Stop Time",
-    "description": "Select a time you would like to begin and end charging."
+    ]
    },
    {
     "name": "page3",
     "elements": [
      {
-      "type": "ranking",
+      "type": "expression",
       "name": "question3",
-      "title": "Is Pikachu your favorite pokemon?",
-      "choices": [
-       {
-        "value": "Item 1",
-        "text": "Yes"
-       },
-       {
-        "value": "Item 2",
-        "text": "Shockingly, yes!"
-       },
-       {
-        "value": "Item 3",
-        "text": "No,  I like to sniff paint."
-       }
-      ]
+      "title": "Pikachu has fried all the circuitry in your electric vehicle."
      }
-    ],
-    "title": "You Must Like Pikachu!",
-    "description": "Or there is something seriously wrong with you! Order them by most agreeable from top to bottom."
+    ]
    },
    {
     "name": "page4",
     "elements": [
      {
-      "type": "dropdown",
-      "name": "question5",
-      "title": "Will you be requiring your EV during this  vacation?",
-      "choices": [
-       {
-        "value": "Item 1",
-        "text": "Yes, I'm going on a roadtrip."
-       },
-       {
-        "value": "Item 2",
-        "text": "No, I will be using other means of transport."
-       }
-      ]
+      "type": "expression",
+      "name": "question4",
+      "title": "Pikachu has fully charged your electric vehicle."
      }
-    ],
-    "title": "Portfi Needed?",
-    "description": "Decide whether you'll need the services of your EV smart charger."
+    ]
    },
    {
     "name": "page5",
     "elements": [
      {
       "type": "expression",
-      "name": "question6",
-      "title": "We hope you have a great experience with Portfi EV smart charging products! Thank you for your patronage."
+      "name": "question5",
+      "title": "Pikachu has put a dent in the side of your vehicle."
      }
     ]
    }
@@ -321,29 +129,31 @@ const json = {
     "type": "complete"
    },
    {
-    "type": "skip",
-    "expression": "{question1} = 'Item 1'",
-    "gotoName": "question2"
+    "type": "complete",
+    "expression": "{question1} = 'Item 1' or {question1} = 'Item 2'"
    },
    {
     "type": "skip",
-    "expression": "{question1} = 'Item 3'",
+    "expression": "{question2} = 'Item 1'",
     "gotoName": "question3"
    },
    {
     "type": "skip",
-    "expression": "{question1} = 'Item 2'",
+    "expression": "{question2} = 'Item 2'",
+    "gotoName": "question4"
+   },
+   {
+    "type": "skip",
+    "expression": "{question2} = 'Item 3'",
     "gotoName": "question5"
    },
    {
-    "type": "skip",
-    "expression": "{question4} anyof ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8', 'Item 9', 'Item 10', 'Item 11', 'Item 12', 'Item 13', 'Item 14', 'Item 15', 'Item 16', 'Item 17', 'Item 18', 'Item 19', 'Item 20', 'Item 21', 'Item 22', 'Item 23', 'Item 24', 'Item 25', 'Item 26', 'Item 27']",
-    "gotoName": "question6"
+    "type": "complete",
+    "expression": "{question3} empty"
    },
    {
-    "type": "skip",
-    "expression": "{question3} notempty",
-    "gotoName": "question6"
+    "type": "complete",
+    "expression": "{question4} empty"
    }
   ]
  };
