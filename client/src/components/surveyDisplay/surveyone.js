140eaf5b13a32465501c91500e0c8c810e5e6212
yxx-dev <hermitlun@gmail.com>
2022-12-13 22:47:43 -0800

    charging modes

--- client/src/components/surveyDisplay/surveyone.js
+++ client/src/components/surveyDisplay/surveyone.js
@@ -1,29 +1,32 @@
 import React, {useState, useCallback} from "react";
 import { useMutation } from '@apollo/client';
 import ChargeNormal from '../ChargingModes/ChargeNormal';
+import ChargeTurbo from '../ChargingModes/ChargeTurbo';
 
 
 import MySurvey from '../surveyTypes/surveytypeone';
 
 const SurveyOne = ({chargerId, portId, maxCurrent}) => {
     const [showPage, setShowPage] = useState(true);
-    
+    const [result, setResult] = useState({});
+
 
     const onCompletePage = useCallback((data) => {
-        console.log(data);
+        console.log(data)
+        setResult(data);
         setShowPage(!showPage)
     },[showPage])
 
-    const setFinalPage = () => {
-        return(
-            <main>
-                <h4>Charging Starting...
+    // const setFinalPage = () => {
+    //     return(
+    //         <main>
+    //             <h4>Charging Starting...
 
                     
-                </h4>
-            </main>
-        )      
-    }
+    //             </h4>
+    //         </main>
+    //     )      
+    // }
 
 
     return(
@@ -32,7 +35,11 @@ const SurveyOne = ({chargerId, portId, maxCurrent}) => {
             showPage?
             <MySurvey showCompletedPage={data=>onCompletePage(data)} /> :
             // setFinalPage()
+            (result.question1==="Item 1" ? 
             <ChargeNormal chargerId={chargerId} portId={portId} maxCurrent={maxCurrent}/>
+            :
+            <ChargeTurbo chargerId={chargerId} portId={portId} maxCurrent={maxCurrent}/>
+            )
             }
         </div>
     )
